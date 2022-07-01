1c4fe70abb3be9c70d85bb6da5f6afbff12ce028
Artur Andruszkiewicz <andruszkiewiczarturmobiledev@gmail.com>
2022-07-01 23:59:23 +0200

    Add sounds effect

--- app/src/main/java/com/example/intervaltimer/Present/Timer.kt
+++ app/src/main/java/com/example/intervaltimer/Present/Timer.kt
@@ -1,5 +1,7 @@
 package com.example.intervaltimer.Present
 
+import android.content.Context
+import android.media.MediaPlayer
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -20,12 +23,16 @@ import com.example.intervaltimer.ui.theme.Green
 import com.example.intervaltimer.ui.theme.Red
 import com.example.intervaltimer.ui.theme.Yellow
 import kotlinx.coroutines.delay
+import com.example.intervaltimer.R.raw.gong
+import com.example.intervaltimer.R.raw.bell_finish
+import com.example.intervaltimer.R.raw.bell_soon
 
 @Composable
 fun Timer(
     navHostController: NavHostController,
     timer: TimerModel
 ) {
+    val context = LocalContext.current
     var rounds by remember { mutableStateOf(timer.rounds) }
     var isWork by remember { mutableStateOf(true) }
     var time by remember { mutableStateOf(timer.startTime) }
@@ -33,8 +40,16 @@ fun Timer(
 
     LaunchedEffect(key1 = time, key2 = isWork) {
         if(isWork) {
+
             delay(1000L)
             time--
+
+            if(option == Option.Round_Time && time == 10) {
+                playAudio(context, bell_soon)
+            } else if(time == 0 && option == Option.Round_Time && rounds > 1) {
+                playAudio(context, bell_finish)
+            }
+
             if(time == 0 && rounds != 0) {
                 when(option) {
                     Option.Start_Time -> {
@@ -44,7 +59,11 @@ fun Timer(
                     Option.Round_Time -> {
                         rounds--
                         option = Option.Delay
-                        time = timer.delay
+                        if(rounds != 0) {
+                            time = timer.delay
+                        } else {
+                            time = 1
+                        }
                     }
                     Option.Delay -> {
                         option = Option.Round_Time
@@ -54,6 +73,7 @@ fun Timer(
             } else if(rounds == 0 && time == 0) {
                 isWork = false
                 option = null
+                playAudio(context, gong)
             }
         }
     }
@@ -108,5 +128,32 @@ fun Timer(
                 text = "${time/60}: ${if(time%60 < 10) "0" + time%60 else time%60}"
             )
         }
+    }
+}
+
+fun playAudio(context: Context, id: Int) {
+
+    var mMediaPlayer: MediaPlayer
+
+    try {
+
+        mMediaPlayer = MediaPlayer()
+        mMediaPlayer = MediaPlayer.create(context, id)
+
+        mMediaPlayer.setOnPreparedListener {
+            mMediaPlayer.start()
+        }
+
+        mMediaPlayer.setOnCompletionListener {
+            mMediaPlayer.release()
+        }
+
+    }
+    catch (e: IllegalArgumentException) {
+        e.printStackTrace()
+    } catch (e: IllegalStateException) {
+        e.printStackTrace()
+    } catch (e: Exception) {
+        e.printStackTrace()
     }
 }
