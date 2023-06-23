48abd0cf6514d04cd55aa21ca322d1ffbd06fbeb
Artur Andruszkiewicz <andruszkiewiczarturmobiledev@gmail.com>
2023-06-23 22:23:56 +0200

    Fix bug with choosing rounds

--- app/src/main/java/com/example/intervaltimer/future_intervalTimer/present/home/compose/Home.kt
+++ app/src/main/java/com/example/intervaltimer/future_intervalTimer/present/home/compose/Home.kt
@@ -5,56 +5,32 @@ package com.example.intervaltimer.future_intervalTimer.present
 import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ContentTransform
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.with
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.SurroundSound
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material.icons.outlined.StarBorder
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.intervaltimer.R
-import com.example.intervaltimer.core.enums.Option
-import com.example.intervaltimer.future_intervalTimer.domain.model.TimerModel
 import com.example.intervaltimer.future_intervalTimer.present.util.screen.Screen
-import com.example.intervaltimer.R.drawable.ic_play
 import com.example.intervaltimer.core.Event.UiEvent
 import com.example.intervaltimer.future_intervalTimer.domain.model.ChooseOptionEnum
-import com.example.intervaltimer.future_intervalTimer.domain.model.OwnIntervalTime
 import com.example.intervaltimer.future_intervalTimer.present.home.HomeEvent
-import com.example.intervaltimer.future_intervalTimer.present.home.compose.timerOption
 import com.example.intervaltimer.future_intervalTimer.present.home.HomeViewModel
 import com.example.intervaltimer.future_intervalTimer.present.home.compose.CurrentChoosePresentation
-import com.maxkeppeker.sheets.core.models.base.BaseSelection
-import com.maxkeppeker.sheets.core.models.base.ButtonStyle
-import com.maxkeppeker.sheets.core.models.base.Header
 import com.maxkeppeker.sheets.core.models.base.SelectionButton
 import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
 import com.maxkeppeler.sheets.duration.DurationDialog
@@ -214,7 +190,7 @@ fun Home(
                 text = "Rounds",
                 isTimer = false
             ) {
-                timerState.show()
+                roundState.show()
             }
 
             Spacer(modifier = Modifier.height(50.dp))
