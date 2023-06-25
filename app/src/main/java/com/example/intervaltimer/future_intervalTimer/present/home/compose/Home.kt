9462fee9780c2707eaaaf769d4b6667fbf291c1e
Artur Andruszkiewicz <andruszkiewiczarturmobiledev@gmail.com>
2023-06-25 23:34:24 +0200

    Change daialog

--- app/src/main/java/com/example/intervaltimer/future_intervalTimer/present/home/compose/Home.kt
+++ app/src/main/java/com/example/intervaltimer/future_intervalTimer/present/home/compose/Home.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -59,8 +60,9 @@ fun Home(
     DurationDialog(
         state = timerState,
         selection = DurationSelection(
+            negativeButton = null,
             positiveButton = SelectionButton(
-                text = "Apply"
+                text = stringResource(id = R.string.Apply)
             ),
             onPositiveClick = {
                 when(option) {
@@ -86,6 +88,7 @@ fun Home(
     DurationDialog(
         state = roundState,
         selection = DurationSelection(
+            negativeButton = null,
             positiveButton = SelectionButton(
                 text = "Apply"
             ),
